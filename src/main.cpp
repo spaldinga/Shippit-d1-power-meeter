1fd9244c8f7ed9debfc0283c25501f6d537c1491
aspaldin <aspaldin@volvocars.com>
2023-09-06 10:01:02 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,121 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+// https://randomnerdtutorials.com/guide-for-oled-display-with-arduino/
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "functions/EmonLib.h"
+// https://simplyexplained.com/blog/Home-Energy-Monitor-ESP32-CT-Sensor-Emonlib/?ref=localhost
+// https://olimex.wordpress.com/2015/09/29/energy-monitoring-with-arduino-and-current-clamp-sensor/
+// https://www.engineersgarage.com/non-invasive-current-sensor-with-arduino/
+
+#define analogPin A0 /* ESP8266 Analog Pin ADC0 = A0 */
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Force EmonLib to use 10bit ADC resolution
+// #define ADC_BITS    10
+// #define ADC_COUNTS  (1<<ADC_BITS)
+
+// ADC_MODE(ADC_TOUT);
+
+// EnergyMonitor emon1;
+int adcValue = 0;  /* Variable to store Output of ADC */
+
+void testdrawchar(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  // Not all the characters will fit on the display. This is normal.
+  // Library will draw what it can and the rest will be clipped.
+  for(int16_t i=0; i<256; i++) {
+    if(i == '\n') display.write(' ');
+    else          display.write(i);
+  }
+
+  display.display();
+  delay(2000);
+}
+
+void testdrawstyles(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Hello, world!"));
+
+  display.setTextColor(BLACK, WHITE); // Draw 'inverse' text
+  display.println(3.141592);
+
+  display.setTextSize(2);             // Draw 2X-scale text
+  display.setTextColor(WHITE);
+  display.print(F("0x")); display.println(0xDEADBEEF, HEX);
+
+  display.display();
+  delay(2000);
+}
+
+void setup()
+{
+  Serial.begin(115200); /* Initialize serial communication at 115200 */
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+    // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  //   Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+//   display.drawPixel(10, 10, WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+  //   display.display() is NOT necessary after every single drawing command,
+  //   unless that's what you want...rather, you can batch up a bunch of
+  //   drawing operations and then update the screen all at once by calling
+  //   display.display(). These examples demonstrate both approaches...
+
+//   testdrawchar();      // Draw characters of the default font
+
+  testdrawstyles();    // Draw 'stylized' characters
+}
+
+void loop()
+{
+  adcValue = analogRead(analogPin); /* Read the Analog Input value */
+ 
+  /* Print the output in the Serial Monitor */
+  Serial.print("ADC Value = ");
+  Serial.println(adcValue);
+  
+  display.clearDisplay();
+
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.print(F("ADC Value = "));
+  display.println(adcValue);
+  display.display();
+
+  delay(300);
+}
